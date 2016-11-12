3d568ec80577f42d0ac3ddfcfb7cd7232eb07e62
Passarinho <passarinho4@gmail.com>
2016-11-12 11:35:33 +0100

    Add Credentials Login View (inputs login&password and submit button). Bind it with Angular controller.

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -5,10 +5,18 @@ angular.module('myApp.view1', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+    controller: 'View1Ctrl',
+    controllerAs: "vm"
   });
 }])
 
 .controller('View1Ctrl', [function() {
+  var vm = this;
+
+  vm.credentialsLogin = credentialsLogin;
+
+  function credentialsLogin() {
+    alert("Login: " + vm.login + " Password: " + vm.password);
+  }
 
 }]);
