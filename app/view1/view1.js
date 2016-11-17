b5f6bdcfdbea7003a7d8b998bfb2ddad229e2bae
Passarinho <passarinho4@gmail.com>
2016-11-17 10:47:41 +0100

    add GET request to authlog. Login by credentials

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,21 +2,33 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl',
-    controllerAs: "vm"
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl',
+            controllerAs: "vm"
+        });
+    }])
 
-.controller('View1Ctrl', [function() {
-  var vm = this;
+    .controller('View1Ctrl', ['$http', function ($http) {
+        var vm = this;
 
-  vm.credentialsLogin = credentialsLogin;
+        vm.credentialsLogin = credentialsLogin;
 
-  function credentialsLogin() {
-    alert("Login: " + vm.login + " Password: " + vm.password);
-  }
+        function credentialsLogin() {
+            alert("Login: " + vm.login + " Password: " + vm.password);
 
-}]);
+            $http.get(
+                "http://tegess.com:8085/api/login/credentials?appId=582ccd5846e0fb00012be767",
+                {headers: {"Authorization": btoa(vm.login + ":" + vm.password)}}
+            ).then(
+                function onSuccess(response) {
+                    console.log(response.data.token);
+                },
+                function onFailure(data) {
+                    console.log(data);
+                }
+            )
+        }
+
+    }]);
